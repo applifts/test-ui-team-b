b5a9247e0f1c3811ca5582e271ce6f4a9a49c8a0
applifts <becky@applifts.com>
2018-01-15 20:07:16 -0600

    Update app.config.js

--- app/app.config.js
+++ app/app.config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var AppConfig = /** @class */ (function () {
     function AppConfig() {
-        this.apiUrl = 'https://testangularwebapi.azurewebsites.net';
+        this.apiUrl = 'https://teamb-webapi.azurewebsites.net';
     }
     return AppConfig;
 }());
